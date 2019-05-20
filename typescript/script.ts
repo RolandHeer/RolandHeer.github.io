481b5bf7105ecc57ded76086181a04a0e1fa0b54
Roland Heer <roland.joachim.heer@hs-furtwangen.de>
2019-05-21 00:00:35 +0200

    penis

--- typescript/script.ts
+++ typescript/script.ts
@@ -1,1 +1,10 @@
 console.log("Konsolentest");
+
+window.onload = function{
+    console.log("das  Fenster wurde geladen")
+    document.getElementById("Penisbutton").addEventListener("click", sagwas);
+}
+
+function sagwas() {
+    console.log("dies ist ein Penisbutton")
+}
