76b9ff5227e85ecb8f3022be9fac5e24364692ff
Roland Heer <roland.joachim.heer@hs-furtwangen.de>
2019-05-21 02:39:04 +0200

    lul

--- Aufgabe_7/script.js
+++ Aufgabe_7/script.js
@@ -1,9 +1,63 @@
+let ButtonZaehler = 0;
+let ZaehlerMax = 10;
+let DieZahl = 0;
+let stepsClassName = 0;
 console.log("Noch is es nicht ganz soweit");
-window.onload = function () {
-    console.log("So, jetzt ist s geladen! - Viel Spaß");
-    document.getElementById("Penisbutton").addEventListener("click", ButtonEvent);
-};
-function ButtonEvent() {
-    console.log("dies ist ein Penisbutton");
+console.log("");
+console.log("////////////////HIER KOMMEN UNSINNIGE RECHNUNGEN//////////////////");
+console.log("");
+console.log(derErsteString + derZweiteString);
+console.log(derErsteString + dieZweiteNummer);
+console.log(dieErsteNummer + derZweiteString);
+console.log(dieErsteNummer + dieZweiteNummer);
+console.log("");
+console.log("///////////////ENDE DER UNSINNIGE RECHNUNGEN//////////////////");
+console.log("");
+function ZaehlerBis() {
+    let zaehler = 0;
+    while (zaehler <= ZaehlerMax) {
+        console.log("" + zaehler);
+        zaehler++;
+    }
+}
+function Clicker() {
+    Zaehler();
+    let changedText = "" + DieZahl;
+    document.getElementById("zahl").innerHTML = changedText;
+}
+function Zaehler() {
+    DieZahl++;
+}
+function ChangeClassName() {
+    let standardText_I = "die Klasse hat sich geaendert in ";
+    let standardText_II = "wenn du s nicht glaubst guck im html Code nach";
+    switch (stepsClassName) { //Der switch ist sehr praktisch wenn man nicht alles mit ifs machen will  ¯\_(ツ)_/¯
+        case 0:
+            stepsClassName++;
+            document.getElementById("Klassenwandler").className = "dieErsteVerwandlung";
+            console.log(standardText_I + document.getElementById("Klassenwandler").className + standardText_II);
+            break;
+        case 1:
+            stepsClassName++;
+            document.getElementById("Klassenwandler").className = "dieZweiteVerwandlung";
+            console.log(standardText_I + document.getElementById("Klassenwandler").className + standardText_II);
+            break;
+        case 2:
+            stepsClassName++;
+            document.getElementById("Klassenwandler").className = "dieDritteVerwandlung";
+            console.log(standardText_I + document.getElementById("Klassenwandler").className + standardText_II);
+            break;
+        case 3:
+            stepsClassName++;
+            document.getElementById("Klassenwandler").className = "dieVierteVerwandlung";
+            console.log(standardText_I + document.getElementById("Klassenwandler").className + standardText_II);
+            break;
+        case 4:
+            stepsClassName = 0;
+            document.getElementById("Klassenwandler").className = "dieLetzteVerwandlung";
+            console.log(standardText_I + document.getElementById("Klassenwandler").className + standardText_II);
+            break;
+        default: console.log("hier ist wohl etwas schief gegangen (・・;)");
+    }
 }
 //# sourceMappingURL=script.js.map
--- Aufgabe_7/script.ts
+++ Aufgabe_7/script.ts
@@ -1,10 +1,62 @@
+let ButtonZaehler: number = 0;
+let ZaehlerMax: number = 10;
+let DieZahl: number = 0;
+let stepsClassName: number = 0;
+
 console.log("Noch is es nicht ganz soweit");
 
-window.onload = function(){
-    console.log("So, jetzt ist s geladen! - Viel Spaß");
-    document.getElementById("Penisbutton").addEventListener("click", ButtonEvent);
+    console.log("")
+    console.log("////////////////HIER KOMMEN UNSINNIGE RECHNUNGEN//////////////////")
+    console.log("")
+    console.log(derErsteString + derZweiteString);
+    console.log(derErsteString + dieZweiteNummer);
+    console.log(dieErsteNummer + derZweiteString);
+    console.log(dieErsteNummer + dieZweiteNummer);
+    console.log("")
+    console.log("///////////////ENDE DER UNSINNIGE RECHNUNGEN//////////////////")
+    console.log("")
 }
 
-function ButtonEvent() {
-    console.log("dies ist ein Penisbutton");
+function ZaehlerBis() {
+    let zaehler: number = 0;
+    while (zaehler <= ZaehlerMax) {
+        console.log("" + zaehler);
+        zaehler++;
+    }
+}
+
+function Clicker() {
+    Zaehler();
+    let changedText: string = "" + DieZahl;
+    document.getElementById("zahl").innerHTML = changedText;
+}
+
+function Zaehler() {
+    DieZahl++;
+}
+
+function ChangeClassName() {
+    let standardText_I: string = "die Klasse hat sich geaendert in ";
+    let standardText_II: string = "wenn du s nicht glaubst guck im html Code nach";
+
+    switch (stepsClassName) {   //Der switch ist sehr praktisch wenn man nicht alles mit ifs machen will  ¯\_(ツ)_/¯
+        case 0: stepsClassName++;
+            document.getElementById("Klassenwandler").className = "dieErsteVerwandlung";
+            console.log(standardText_I + document.getElementById("Klassenwandler").className + standardText_II);
+            break;
+        case 1: stepsClassName++;
+            document.getElementById("Klassenwandler").className = "dieZweiteVerwandlung";
+            console.log(standardText_I + document.getElementById("Klassenwandler").className + standardText_II); break;
+        case 2: stepsClassName++;
+            document.getElementById("Klassenwandler").className = "dieDritteVerwandlung";
+            console.log(standardText_I + document.getElementById("Klassenwandler").className + standardText_II); break;
+        case 3: stepsClassName++;
+            document.getElementById("Klassenwandler").className = "dieVierteVerwandlung";
+            console.log(standardText_I + document.getElementById("Klassenwandler").className + standardText_II); break;
+        case 4: stepsClassName = 0;
+            document.getElementById("Klassenwandler").className = "dieLetzteVerwandlung";
+            console.log(standardText_I + document.getElementById("Klassenwandler").className + standardText_II); break;
+
+        default: console.log("hier ist wohl etwas schief gegangen (・・;)")
+    }
 }
