d4789862fd58835038a0f7a9d362aedb9a0c5884
Roland Heer <roland.joachim.heer@hs-furtwangen.de>
2019-11-16 17:58:40 +0100

    lkaf

--- Scheiß/prog.ts
+++ Scheiß/prog.ts
@@ -1,1 +1,13 @@
-console.log("Lol so ein kack")
+let variable:String = "Humbug";
+
+console.log(variable);
+console.log("Lol so ein kack");
+console.log("start");
+communicate("https://jirkadelloro.github.io/EIA2-Inverted/L05_Client/Material/Test.txt");
+console.log("end");
+async function communicate(_url: RequestInfo): Promise<void> {
+    let response: Response = await fetch(_url);
+    console.log("Response", response);
+    variable = await response.text();
+    console.log(variable);
+}
