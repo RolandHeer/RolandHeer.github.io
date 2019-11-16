33178f5419826a69b34edc65e00d68fd46b4b4d4
Roland Heer <roland.joachim.heer@hs-furtwangen.de>
2019-11-16 21:42:08 +0100

    asfd

--- MetamorphusWeihnachtsmarkt-TestSite/Inner.js
+++ MetamorphusWeihnachtsmarkt-TestSite/Inner.js
@@ -1,2 +1,14 @@
-console.log("Ich bin des Weihnachtsmarktes - Inneres - Programm");
+var code;
+(function (code) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        console.log("hat geladen");
+        main();
+    }
+    function main() {
+        console.log("bin im Main");
+        let parentDocument = window.parent.document;
+        // console.log("dies ist die Ausgabe: " + parentDocument.getElementById("ueberschrift").innerHTML);  
+    }
+})(code || (code = {}));
 //# sourceMappingURL=Inner.js.map
--- MetamorphusWeihnachtsmarkt-TestSite/Inner.ts
+++ MetamorphusWeihnachtsmarkt-TestSite/Inner.ts
@@ -1,1 +1,19 @@
-console.log("Ich bin des Weihnachtsmarktes - Inneres - Programm");
+namespace code{
+
+    window.addEventListener("load",handleLoad);
+
+    function handleLoad(){
+        console.log("hat geladen");
+        main();
+    }
+
+    function main (){
+        console.log("bin im Main");
+        
+        let parentDocument : HTMLDocument = window.parent.document;
+        
+        
+
+       // console.log("dies ist die Ausgabe: " + parentDocument.getElementById("ueberschrift").innerHTML);  
+    }
+}
